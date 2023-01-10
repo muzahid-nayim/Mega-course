7814e57725786d77258fa4feacd9f933e62d0ec6
muzahid-nayim <muzahidulislamnayim@gmail.com>
2023-01-10 14:05:18 +0600

    mega course

--- JS/app.js
+++ JS/app.js
@@ -17,19 +17,20 @@ window.addEventListener('scroll', function() {
 });
 
 // ===          SWIPER            ====
+
 var swiper = new Swiper(".mySwiper", {
-   spaceBetween: 30,
-   centeredSlides: true,
-   autoplay: {
-     delay: 2500,
-     disableOnInteraction: false,
-   },
-   pagination: {
-     el: ".swiper-pagination",
-     clickable: true,
-   },
-   navigation: {
-     nextEl: ".swiper-button-next",
-     prevEl: ".swiper-button-prev",
-   },
- });
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
